97fbc70fbcabbb93947509868c3b209d791f5790
stevoh <stepheneokeyo64@gmail.com>
2023-06-24 13:52:29 -0700

    update 2

---
+++ emails/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import InventoryWaitlist
+
+admin.site.register(InventoryWaitlist)
+from django.contrib import admin
+
+# Register your models here.
---
+++ products/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from products.models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'inventory/product_list.html', {'products': products})
+
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    if product.inventory > 0:
+        product.decrease_inventory(1)
+        # Perform any additional logic here, e.g., add the product to the customer's cart
+        return redirect('product_list')
+    else:
+        return render(request, 'inventory/out_of_stock.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Product.CATEGORY_CHOICES
+    return render(request, 'inventory/product_list.html', {'products': products, 'categories': categories})
+
